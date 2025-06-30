8b80cb82690affba6865706a7dd898fa7f5ee99d
Shepherd Yaw Morttey <42776664+smorttey@users.noreply.github.com>
2025-06-30 22:19:20 +0000

    Expand rails_helper and add User email spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without an email' do
+    user = User.new(password: 'password123', password_confirmation: 'password123')
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to be_present
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,18 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+
+  # You can uncomment this line to turn off ActiveRecord support entirely.
+  # config.use_active_record = false
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
